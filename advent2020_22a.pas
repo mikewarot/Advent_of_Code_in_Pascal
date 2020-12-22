f64ac03ea8fece3206b0fcc06669819a16438267
Mike Warot <chezmike@gmail.com>
2020-12-22 05:10:56 -0600

    Spent way too much time over-engineering part 1 of Day 22... had to scrap it to get part 2

---
+++ advent2020_22a.pas
@@ -0,0 +1,255 @@
+program advent2020_22a;
+uses
+  classes, sysutils, dateutils;
+type
+  charset = set of char;
+const
+  whitespace : charset = [' ',#9,#10,#13];
+
+procedure eat(var s : string;  whattoeat : charset);
+begin
+  while (length(s) <> 0) and (s[1] in whattoeat) do
+    delete(s,1,1);
+end;
+
+procedure Expect(var s : string; c : char);
+begin
+  if (s='') OR (s[1] <> c) then
+    writeln('Error : Expected [',c,'], Got [',s,']')
+  else
+    delete(s,1,1);
+end;
+
+function getnum(var s : string):integer;
+var
+  c : char;
+  value : integer;
+begin
+  value := 0;
+  eat(s,whitespace);
+  while (length(s) <> 0) and (s[1] in ['0'..'9']) do
+  begin
+    value := value * 10 + (ord(s[1])-ord('0'));
+    delete(s,1,1);
+  end;
+  eat(s,whitespace);
+  getnum := value;
+end;
+
+function getinteger(var s : string):integer;
+var
+  value : integer;
+  sign  : integer;
+begin
+  value := 0;
+  sign := 1;
+  eat(s,whitespace);
+  if s <> '' then
+    if s[1] = '+' then
+      delete(s,1,1)
+    else
+      if s[1] = '-' then
+      begin
+        sign := -1;
+        delete(s,1,1);
+      end;
+  while (s <> '') AND (s[1] in ['0'..'9']) do
+  begin
+    value := (value * 10) + (ord(s[1])-ord('0'));
+    delete(s,1,1);
+  end;
+  value := value * sign;
+  getinteger := value;
+end;
+
+function getword(var s : string):string;
+var
+  c : char;
+  value : string;
+begin
+  value := '';
+  while (length(s) <> 0) and (s[1] in [' ']) do
+    delete(s,1,1);
+  while (length(s) <> 0) and (s[1] in ['0'..'9','a'..'z','A'..'Z']) do
+  begin
+    value := value + s[1];
+    delete(s,1,1);
+  end;
+  while (s <> '') AND (s[1] = ' ') do
+    delete(s,1,1);
+  getword := value;
+end;
+
+  Function Reverse(T : String):String;
+  var
+    i : integer;
+    s : string;
+  begin
+    s := '';
+    for i := length(t) downto 1 do
+      s := s + t[i];
+    Reverse := s;
+  end;
+
+var
+  CardCount    : integer;
+  CardSequence : integer;
+  CardValue    : array[1..10000] of integer;
+  CardOwner    : array[1..10000] of integer;
+  CardOrder    : array[1..10000] of integer;
+  playerfirstcard : array[1..10] of integer;
+  playerCardCount : array[1..10] of integer;
+  playerName   : array[1..10] of string;
+  PlayerCount  : integer;
+
+
+var
+  StartTime  : TDateTime;
+  done       : boolean;
+  s,t        : string;
+  i,j,k,m,n  : integer;
+  Card       : integer;
+  Player,
+  TurnWinner,
+  TurnCardCount : Integer;
+  GameWinner : Integer;
+
+  procedure DumpCards;
+  var
+    SumOfCards  : integer;
+    PlayerScore : integer;
+  begin
+    playerscore := 0;
+    SumOfCards  := 0;
+    for Player := 1 to playercount do
+      begin
+        Write('Player (',Player,') ',PlayerName[Player],' : ');
+        for i := 0 to CardSequence do
+          for Card := 1 to CardCount do
+            if (CardOrder[Card] = i) and  (CardOwner[Card] = Player) then
+              begin
+                Inc(SumOfCards,CardValue[Card]);
+                Inc(PlayerScore,SumOfCards);
+                Write(CardValue[Card],' ');
+              end;
+
+        WriteLn(' Score = ',PlayerScore);
+      end;
+  end;
+
+begin
+  StartTime := Now;
+  done := false;
+  cardcount := 0;
+  cardSequence := 0;
+  playercount := 0;
+
+  writeln('Input Section');
+  repeat
+    readln(s);
+    if GetWord(s) = 'Player' then
+    begin
+      Inc(PlayerCount);
+      PlayerName[PlayerCount] := GetWord(s);
+      PlayerFirstCard[PlayerCount] := CardCount+1;
+      PlayerCardCount[PlayerCount] := 0;
+      Expect(s,':');
+      repeat
+        readln(s);
+        if s <> '' then
+        begin
+//          WriteLn('Card: [',s,']');
+          Inc(CardCount);
+          Inc(CardSequence);
+          Inc(PlayerCardCount[PlayerCount]);
+          CardValue[CardCount] := GetInteger(s);
+          CardOwner[CardCount] := PlayerCount;   // the current player owns this card
+          CardOrder[CardCount] := 0;             // while we're dealing, all cards are "new"
+        end
+        else
+          done := true;
+//        writeLn('Leftover: ',s);
+      until done or eof;
+      done := false;
+    end;
+  until eof;
+
+  WriteLn(PlayerCount,' Players');
+  for i := 1 to playercount do
+    WriteLn('Player(',i,') is ',PlayerName[i],' with ',PlayerCardCount[i],' cards.');
+  WriteLn(CardCount,' Cards accepted');
+  DumpCards;
+
+  writeln;
+  writeln('Processing Section');
+  writeln;
+
+  Done := False;
+  n := 10000;
+  repeat
+
+    TurnWinner := 0;
+    TurnCardCount := 0;
+    For Player := 1 to PlayerCount do
+      If PlayerCardCount[Player] > 0 then
+      begin
+        If TurnWinner = 0 then
+          TurnWinner := Player;   // handle player with no cards
+        Inc(TurnCardCount);       // how many cards in this round?
+        WriteLn('  Player (',Player,') plays ',CardValue[PlayerFirstCard[Player]]);
+        If CardValue[PlayerFirstCard[Player]] > CardValue[PlayerFirstCard[TurnWinner]] then
+          TurnWinner := Player;   // actual test to see who has the highest value, on tie goes to first player with card
+      end;
+    WriteLn('Winner Is Player(',TurnWinner,') who gets ',TurnCardCount,' cards');
+
+    DumpCards;
+
+    Inc(CardSequence);
+    CardOrder[PlayerFirstCard[TurnWinner]] := CardSequence;
+    For Player := 1 to PlayerCount do
+      If (PlayerCardCount[Player] > 0) AND (Player <> TurnWinner) then  // skip everone out of the game
+      begin
+        Inc(CardSequence);                 // use this to order cards once they get scrambled
+        CardOwner[PlayerFirstCard[Player]] := TurnWinner;   // assign card to the Winner (even if same player)
+        CardOrder[PlayerFirstCard[Player]] := CardSequence; // give it a new order number
+        Dec(PlayerCardCount[Player]);                       // remove the card from the owners count
+        Inc(PlayerCardCount[TurnWinner]);                   // add it to the winners count
+      end;
+
+    DumpCards;
+
+    // Find first card for all players left, despite scrambling from multiple loops
+    For Player := 1 to PlayerCount do
+      If PlayerCardCount[Player] > 0 then  // skip everone out of the game
+        begin
+          PlayerFirstCard[Player] := 0;
+          for Card := 1 to CardCount do
+            If (CardOwner[Card] = Player) then
+              begin
+                If PlayerFirstCard[Player] = 0 then
+                  PlayerFirstCard[Player] := Card;
+                If CardOrder[Card] < CardOrder[PlayerFirstCard[Player]] then
+                  PlayerFirstCard[Player] := Card;
+              end;
+        end;
+
+    DumpCards;
+
+    WriteLn('Current Statistics');
+    For Player := 1 to PlayerCOunt do
+      begin
+        WriteLn('Player (',Player,') has ',PlayerCardCount[Player],' Cards');
+        If PlayerCardCount[Player] = CardCount then
+          begin
+            WriteLn('Player (',Player,') ',PlayerName[Player],' is the winner!');
+            Done := True;
+          end;
+      end;
+    dec(n);
+    if n <= 0 then
+      done := true;
+  until done;
+
+  writeln((MilliSecondsBetween(Now,StartTime)*0.001):10:3,' seconds');
+end.
+
---
+++ advent2020_22b.pas
@@ -0,0 +1,241 @@
+program advent2020_22b;
+uses
+  classes, sysutils, dateutils;
+type
+  charset = set of char;
+const
+  whitespace : charset = [' ',#9,#10,#13];
+
+procedure eat(var s : string;  whattoeat : charset);
+begin
+  while (length(s) <> 0) and (s[1] in whattoeat) do
+    delete(s,1,1);
+end;
+
+procedure Expect(var s : string; c : char);
+begin
+  if (s='') OR (s[1] <> c) then
+    writeln('Error : Expected [',c,'], Got [',s,']')
+  else
+    delete(s,1,1);
+end;
+
+function getnum(var s : string):integer;
+var
+  c : char;
+  value : integer;
+begin
+  value := 0;
+  eat(s,whitespace);
+  while (length(s) <> 0) and (s[1] in ['0'..'9']) do
+  begin
+    value := value * 10 + (ord(s[1])-ord('0'));
+    delete(s,1,1);
+  end;
+  eat(s,whitespace);
+  getnum := value;
+end;
+
+function getinteger(var s : string):integer;
+var
+  value : integer;
+  sign  : integer;
+begin
+  value := 0;
+  sign := 1;
+  eat(s,whitespace);
+  if s <> '' then
+    if s[1] = '+' then
+      delete(s,1,1)
+    else
+      if s[1] = '-' then
+      begin
+        sign := -1;
+        delete(s,1,1);
+      end;
+  while (s <> '') AND (s[1] in ['0'..'9']) do
+  begin
+    value := (value * 10) + (ord(s[1])-ord('0'));
+    delete(s,1,1);
+  end;
+  value := value * sign;
+  getinteger := value;
+end;
+
+function getword(var s : string):string;
+var
+  c : char;
+  value : string;
+begin
+  value := '';
+  while (length(s) <> 0) and (s[1] in [' ']) do
+    delete(s,1,1);
+  while (length(s) <> 0) and (s[1] in ['0'..'9','a'..'z','A'..'Z']) do
+  begin
+    value := value + s[1];
+    delete(s,1,1);
+  end;
+  while (s <> '') AND (s[1] = ' ') do
+    delete(s,1,1);
+  getword := value;
+end;
+
+  Function Reverse(T : String):String;
+  var
+    i : integer;
+    s : string;
+  begin
+    s := '';
+    for i := length(t) downto 1 do
+      s := s + t[i];
+    Reverse := s;
+  end;
+
+  Function WordCount(S : String):Integer;
+  var
+    value : integer;
+  begin
+    value := 0;
+    while getword(s) <> '' do
+      inc(value);
+    WordCount := value;
+  end;
+
+  Function Score(Deck : String):Integer;
+  var
+    SumOfCards : Integer;
+    TotalScore : integer;
+  begin
+    SumOfCards := 0;
+    TotalScore := 0;
+    While Deck <> '' do
+    begin
+      Inc(SumOfCards,GetNum(Deck));
+      Inc(TotalScore,SumofCards);
+    end;
+    Score := TotalScore;
+  end;
+
+  Function PlayGame(Deck1,Deck2 : String):Integer;
+  var
+    Turns      : Array[1..10000] of string;
+    TurnCount  : Integer;
+    Card1,Card2 : Integer;
+    Winner     : Integer;
+    PreviousTurn : Integer;
+    NewDeck1,
+    NewDeck2    : String;
+    S : String;
+    i : integer;
+  begin
+    WriteLn('FunctionPlayGame');
+    WriteLn('Player 1 : ',Deck1);
+    WriteLn('Player 2 : ',Deck2);
+
+    TurnCount := 0;
+
+    Repeat
+      Inc(TurnCount);
+      Turns[TurnCount] := Deck1+' : '+Deck2;
+      WriteLn('Turn [',TurnCount,'] Saved : ',Turns[TurnCount]);
+
+      Winner := 0;
+      for PreviousTurn := 1 to TurnCount-1 do
+        if (Turns[PreviousTurn] = Turns[TurnCount]) then
+          begin
+            WriteLn('Matched previous turn #',PreviousTurn);
+            Winner := 1;
+          end;
+
+      Card1 := GetNum(Deck1);
+      Card2 := GetNum(Deck2);
+
+      If Winner = 1 then
+        Deck1 := Deck1 + ' ' + Card1.ToString + ' ' + Card2.ToString
+      else
+        begin
+          if (Card1 <= WordCount(Deck1)) AND (Card2 <= WordCount(Deck2)) then
+            begin
+              WriteLn('Recurse!');
+              S := Deck1;  NewDeck1 := GetWord(s); for i := 2 to Card1 do NewDeck1 := NewDeck1 + ' ' + GetWord(s);
+              S := Deck2;  NewDeck2 := GetWord(s); for i := 2 to Card2 do NewDeck2 := NewDeck2 + ' ' + GetWord(s);
+              Winner := PlayGame(NewDeck1,NewDeck2);
+              If Winner = 1 then
+                Deck1 := Deck1 + ' ' + Card1.ToString + ' ' + Card2.ToString
+              else
+                Deck2 := Deck2 + ' ' + Card2.ToString + ' ' + Card1.ToString;
+              Winner := 0;
+            end
+          else
+            If Card1 > Card2 then
+              Deck1 := Deck1 + ' ' + Card1.ToString + ' ' + Card2.ToString
+            else
+              Deck2 := Deck2 + ' ' + Card2.ToString + ' ' + Card1.ToString;
+        end;
+
+      If Deck1 = '' then Winner := 2;
+      If Deck2 = '' then Winner := 1;
+    until Winner <> 0;
+
+    WriteLn('Winner = ',Winner);
+    WriteLn('Deck 1 (',Score(Deck1),' points): ',Deck1);
+    WriteLn('Deck 2 (',Score(Deck2),' points): ',Deck2);
+    WriteLn('EXIT PlayGame');
+    PlayGame := Winner;
+  end;
+
+
+var
+  StartTime  : TDateTime;
+  done       : boolean;
+  s,t        : string;
+  i,j,k,m,n  : integer;
+  Deck1,Deck2 : String;
+  Winner     : Integer;
+
+begin
+  StartTime := Now;
+  done := false;
+
+  writeln('Input Section');
+  readln(s);
+  readln(Deck1);
+  repeat
+    readln(s);
+    if s <> '' then
+    begin
+      Deck1 := Deck1 + ' ' + S;
+    end
+    else
+      done  := true;
+  until done or eof;
+
+  done := false;
+  readln(s);
+  readln(Deck2);
+  repeat
+    readln(s);
+    if s <> '' then
+    begin
+      Deck2 := Deck2 + ' ' + S;
+    end
+    else
+      done := true;
+  until done or eof;
+
+  WriteLn('Player 1 : ',Deck1);
+  WriteLn('Player 2 : ',Deck2);
+
+  writeln;
+  writeln('Processing Section');
+  writeln;
+
+  Winner := PlayGame(Deck1,Deck2);
+  WriteLn('Winner = ',Winner);
+
+  WriteLn('Deck 1 : ',Deck1);
+  WriteLn('Deck 2 : ',Deck2);
+
+  writeln((MilliSecondsBetween(Now,StartTime)*0.001):10:3,' seconds');
+end.
+
