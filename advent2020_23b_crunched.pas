692692c17ef53e1ccb82483c9b99e1b99242e8c2
Mike Warot <chezmike@gmail.com>
2020-12-23 20:03:20 -0600

    Used a file of longint, and made it work in Turbo Pascal 7.0 8)

--- advent2020_23b_crunched.pas
+++ advent2020_23b_crunched.pas
@@ -1,34 +1,18 @@
-program advent2020_23b;
+program advent2020_23b_crunched;
 uses
   sysutils, dateutils;
-
 const
   MaxTerm    = 1000000;
   TurnCount  = 10000000;
-
 var
   StartTime  : TDateTime;
   s          : string;
-  i          : integer;
-
   Next       : Array[1..MaxTerm] of integer;    // a singly linked list, of sorts
   Pickup     : Array[1..3] of integer;
-  Target     : Integer;
-  Turn       : Integer;
-  X          : Integer;
-  TermCount  : Integer;
-
-  First,
-  Last       : Integer;
-
-  Pickup1    : Integer;
-
-
+  i,Target, Turn, X, TermCount, First, Last, Pickup1     : Integer;
 begin
   StartTime := Now;
-
-  last := 1;
-
+  last := 1; first := 1;
   readln(S);
   for i := 1 to length(s) do
     begin
@@ -40,20 +24,13 @@ begin
       last := x;
       termcount := i;
     end;
-
   for i := termcount+1 to MaxTerm do
     begin
       next[last] := i;
       last := i;
-      termcount := i;
     end;
-
+  TermCount := MaxTerm;
   next[last] := first;   // make it into a ring!
-
-  writeln;
-  writeln('Processing Section');
-  writeln;
-
   x := first;
   for i := 1 to 10 do
     begin
@@ -61,7 +38,6 @@ begin
       x := next[x];
     end;
   writeln;
-
   for turn := 1 to turncount do
     begin
       target  := first-1;     if target < 1 then target := termcount;
@@ -71,18 +47,15 @@ begin
       pickup[3]   := next[pickup[2]];
       next[first] := next[pickup[3]];
       first       := next[first];
-
       while (target = pickup[1]) or
             (target = pickup[2]) or
             (target = pickup[3]) do
         begin
           dec(target);  if target = 0 then target := termcount;
         end;
-
       next[pickup[3]] :=  next[target];
       next[target] := pickup[1];
     end; // for turn
-
   x := next[1];
   for i := 1 to 10 do
     begin
@@ -90,9 +63,7 @@ begin
       x := next[x];
     end;
   writeln;
-
   WriteLn('Product = ',Next[1] * Next[Next[1]]);
-
   writeln((MilliSecondsBetween(Now,StartTime)*0.001):10:3,' seconds');
 end.
 
